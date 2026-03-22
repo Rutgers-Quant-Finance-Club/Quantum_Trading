472f61b787699cebd99bef7b0f40abf1c01c0f7b
Joel Pulikkan <joelpulikkan253@gmail.com>
2026-03-22 20:56:42 +0000

    Created a circuit

--- codebase/circuit.py
+++ codebase/circuit.py
@@ -1,7 +1,18 @@
 from qiskit import QuantumCircuit
+from qiskit.primitives import StatevectorSampler
 from qiskit.visualization import plot_histogram
+import matplotlib.pyplot as plt
 
+qc = QuantumCircuit(2)
+qc.h(0)
+qc.cx(0, 1)
+qc.measure_all()
 
+sampler = StatevectorSampler()
+result = sampler.run([qc], shots=100000).result()
 
-def __main__():
-    print("Hello, World!")
+counts = result[0].data.meas.get_counts()
+print(counts)
+
+plot_histogram(counts)
+plt.show()
